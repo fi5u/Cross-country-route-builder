3b600d68b8603cef6e0310d3f4f91a90ebc577a3
Thomas Fisher <thomasbfisher@yahoo.com>
2022-02-27 08:52:53 +0200

    Map: updated zoom level

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -7,15 +7,18 @@ import styles from "./Map.module.css";
 import config from "config";
 
 /**
- *
- * @param param.
+ * Outputs a map with supplied markers/polyline
+ * @param param.markers Waypoint markers to place on the map
+ * @param param.onMapClick Callback to call when map is clicked
+ * @param param.polyline Polyline to visibly connect the waypoints
  */
 function Map({ markers, onMapClick, polyline }: Props) {
   const map = useRef<L.Map>();
   const markerGroup = useRef<L.LayerGroup | null>(L.layerGroup(markers));
 
+  // On first render, set up the map with view/zoom/etc settings
   useEffect(() => {
-    map.current = L.map("map").setView([51.505, -0.09], 13);
+    map.current = L.map("map").setView([51.505, -0.09], 15);
 
     L.tileLayer(
       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -24,13 +27,14 @@ function Map({ markers, onMapClick, polyline }: Props) {
         attribution:
           'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         id: "mapbox/streets-v11",
-        maxZoom: 10,
+        maxZoom: 20,
         tileSize: 512,
         zoomOffset: -1,
       }
     ).addTo(map.current);
 
     map.current.on("click", onMapClick);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // When `markers` change, first remove current markers and update the list
